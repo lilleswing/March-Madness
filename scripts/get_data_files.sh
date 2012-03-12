4ad76043fe95150658cfce898b2d1e4e3e93eb27
Karl Leswing <lilleswing.com>
2012-03-11 23:32:04 -0400

    wrote the regex to pull team statistics out of the 4 different html files for height, four factors, misc, offensive %s

--- scripts/get_data_files.sh
+++ scripts/get_data_files.sh
@@ -1,6 +1,34 @@
+datapath=~/Documents/CompSci/March_Madness/data
+
+
+mkdir "$datapath/team_data"
+mkdir "$datapath/player_data"
+mkdir "$datapath/stat_data"
+mkdir "$datapath/stat_data/height"
+mkdir "$datapath/stat_data/stats"
+mkdir "$datapath/stat_data/summary"
+mkdir "$datapath/stat_data/teamstats"
+mkdir "$datapath/team_csv"
+mkdir "$datapath/player_csv"
+mkdir "$datapath/sta_csv"
+
+
+
 python get_team_names.py
 ruby get_team_data.rb
-mv *team=* ../data/team_data
+mv *team=* "$datapath/team_data"
 ruby get_player_data.rb
-mv *team=* ../data/player_data
-python parse_player_data.py ~/Documents/CompSci/March_Madness/data/player_data/
+mv *team=* "$datapath/player_data"
+ruby get_stats.rb
+mv height* "$datapath/stat_data/height"
+mv stats* "$datapath/stat_data/stats"
+mv summary* "$datapath/stat_data/summary"
+mv teamstats* "$datapath/stat_data/teamstats"
+python parse_player_data.py "$datapath/player_data"
+python parse_team_data.py "$datapath/team_data"
+python parse_stat_data.py "$datapath/stat_data/height" 9 "height"
+python parse_stat_data.py "$datapath/stat_data/teamstats" 7 "teamstats"
+python parse_stat_data.py "$datapath/stat_data/summary" 6 "summary"
+python parse_stat_data.py "$datapath/stat_data/stats" 11 "stats"
+
+
