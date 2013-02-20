7d3590dae688d19f4dd8906dcd346297dd26e9ec
karl <lilleswing@gmail.com>
2013-02-20 11:44:20 -0500

    cleaning up data, getting rid of the files for inode usage sake.

--- scripts/get_player_data.rb
+++
@@ -1,4 +0,0 @@
-for i in 0...15000
-	url = "http://www.kenpom.com/player.php?p=#{i}"
-	`wget --load-cookies cookies.txt #{url}`
-end
--- scripts/get_stats.rb
+++
@@ -1,7 +0,0 @@
-stats = ["height.php?","teamstats.php?","summary.php?","stats.php?"]
-for stat in stats
-	for year in 2003..2012
-		url = "http://www.kenpom.com/#{stat}y=#{year}"
-		`wget --load-cookies cookies.txt #{url}`
-	end
-end
