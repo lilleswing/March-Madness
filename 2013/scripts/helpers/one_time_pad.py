0c2f54b7ea07103b413e288fc8ba982e360959e5
karl <lilleswing@gmail.com>
2013-03-15 09:51:44 -0400

    Added Encrypted database and one_time_pad file to unencrypt it.

---
+++ 2013/scripts/helpers/one_time_pad.py
@@ -0,0 +1,21 @@
+__author__ = 'karl_leswing'
+import sys
+import os
+
+
+def cipher(infile, outfile, padfile):
+    block_size = 65536
+    while 1:
+        data = infile.read(block_size)
+        if not data:
+            break
+        pad = padfile.read(len(data))
+        encoded = ''.join([ chr(ord(a) ^ ord(b)) for a, b in zip(data, pad) ])
+        outfile.write(encoded)
+
+
+if __name__ == '__main__':
+    infile = sys.argv[1]
+    padfile = sys.argv[2]
+    outfile = os.path.join(os.path.dirname(os.path.abspath(infile)), 'kenpom_restored.db')
+    cipher(open(infile), open(outfile, 'wb'), open(padfile))
